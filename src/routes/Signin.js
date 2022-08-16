6d784a431ab60b8b165057bf0a47fd3cc03d295a
crypto523 <jacksoncurry46@gmail.com>
2022-08-16 13:10:34 +0530

    added signup route

--- src/routes/Signin.js
+++ src/routes/Signin.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { AiFillLock, AiOutlineMail } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
-	return <div>Signin</div>;
+	return (
+		<div>
+			<div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
+				<h1 className="text-2xl font-bold">Sign In</h1>
+				<form>
+					<div className="my-4">
+						<label>Email</label>
+						<div className="my-2 w-full relative rounded-2xl shadow-xl">
+							<input
+								type="email"
+								className="w-full p-2 bg-primary border border-input rounded-2xl"
+							/>
+							<AiOutlineMail className="absolute right-2 top-3 text-gray-400" />
+						</div>
+					</div>
+					<div>
+						<label>Password</label>
+						<div className="my-2 w-full relative rounded-2xl shadow-xl">
+							<input
+								type="password"
+								className="w-full p-2 bg-primary border border-input rounded-2xl"
+							/>
+							<AiFillLock className="absolute right-2 top-3 text-gray-400" />
+						</div>
+					</div>
+					<button className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
+						Sign in
+					</button>
+				</form>
+				<p className="my-4">
+					Don't have an account?{" "}
+					<Link className="text-accent" to="/signup">
+						Sign up
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Signin;
